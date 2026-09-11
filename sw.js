709837202a925a7b12cdc4e0cdc72bd2ba9570fe
roninriddle <63179553+roninriddle@users.noreply.github.com>
2026-09-11 11:45:39 +0800

    feat: add PWA service worker

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE='wloc-shell-v1';
+const SHELL=['./','./index.html','./manifest.json','./icon.svg'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET')return;
+  const url=new URL(req.url);
+  if(url.origin!==self.location.origin)return;
+  if(req.mode==='navigate'){
+    event.respondWith(fetch(req).then(res=>{
+      const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(req).then(hit=>hit||fetch(req).then(res=>{
+    const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res;
+  })));
+});
